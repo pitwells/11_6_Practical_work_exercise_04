b0fa6bc8faabff6886dadaa78c7115da96876642
Дмитрий Богданов <64403847+DmitriyBogdanov@users.noreply.github.com>
2022-10-08 22:53:36 +0600

    функция проверки ввода символов

--- main.cpp
+++ main.cpp
@@ -2,34 +2,55 @@
 
 // функция получения клетки поля по координатам - типа готово
 // функция определения победителя
+// функция корректности поля
+// функция проверки ввода символов +
+std::string result;
+std::string line_1, line_2, line_3;
+std::string symbols = "XO.";
 
-std::string getCellValue (std::string x, int y) {
+std::string checkCorrectSymbols(std::string line) {
+    int check = 0;
+    bool qw = true;
+    do {
+        std::cout << "Enter the value of the line:" << std::endl;
+        std::cin >> line;
+        if (line.length() == 3) {
+            check = 1;
+        } else {
+            check = 0;
+            std::cout << "The string must be no more than three characters" << std::endl;
+        }
 
-    std::string w;
-    w = x[y];
-    return w;
+        if (check == 1) {
+            for (int i = 0; i < line.length() ; i++) {
+                int ck = 1;
+                for (int j = 0; j < symbols.length(); j++) {
+                    if (line[i] == symbols[j]) {
+                        ck = 1;
+                        break;
+                    } else {
+                        ck = -1;
+                    }
+                }
+                check += ck;
+            }
+            if (check == 4) qw = false;
+
+        }
+    } while (qw);
+
+    return line;
 }
-
-std::string checkWin (std::string q, std::string w) {
-    return "0";
-}
-
 
 int main() {
     std::cout << "Evaluation of the results of the game of tic-tac-toe" << std::endl << std::endl;
 
-    std::string str1, str2, str3;
-    std::cout << "Enter the value of the first line:" << std::endl;
-    std::cin >> str1;
-    std::cout << "Enter the value of the second line:" << std::endl;
-    std::cin >> str2;
-    std::cout << "Enter the value of the third line:" << std::endl;
-    std::cin >> str3;
+    checkCorrectSymbols(line_1);
+    checkCorrectSymbols(line_2);
+    checkCorrectSymbols(line_3);
 
-    std::cout << str1 << std::endl << str2 << std::endl << str3 << std::endl;
+    std::cout << line_1;
+    std::cout << "EEEOX.";
 
-    std::cout << getCellValue(str1,2);
-    if (getCellValue(str1,2) == getCellValue(str2,1)) std::cout << "YES" << std::endl;
-        else std::cout << "NO" << std::endl;
     return 0;
 }
